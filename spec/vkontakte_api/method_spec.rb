c5796186e3856099c8b164bbc199bcf496c746d3
Vsevolod Romashov <7@7vn.ru>
2012-06-28 02:33:29 +0400

    1.8 fix

--- spec/vkontakte_api/method_spec.rb
+++ spec/vkontakte_api/method_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe VkontakteApi::Method do
   it "should remember the namespace" do
-    method = VkontakteApi::Method.new('name', namespace: 'namespace')
+    method = VkontakteApi::Method.new('name', :namespace => 'namespace')
     method.namespace.should == 'namespace'
   end
 end
