5fb71d9bb6139dae3114262990a8928c66c77b25
Vsevolod Romashov <7@7vn.ru>
2012-06-28 05:07:32 +0400

    several methods transferred from VK::Resolver to VK::Method

--- lib/vkontakte_api/method.rb
+++ lib/vkontakte_api/method.rb
@@ -1,5 +1,31 @@
 module VkontakteApi
   class Method
     include Resolvable
+    
+    # A pattern for names of methods with a boolean result.
+    PREDICATE_NAMES = /^is.*\?$/
+    
+  private
+    def full_name
+      parts = [@resolver.name, @name].compact.map { |part| camelize(part) }
+      parts.join('.').gsub(/[^A-Za-z.]/, '')
+    end
+    
+    def type
+      @name =~ PREDICATE_NAMES ? :boolean : :anything
+    end
+    
+    # camelize('get_profiles')
+    # => 'getProfiles'
+    def camelize(name)
+      words = name.split('_')
+      first_word = words.shift
+      
+      words.each do |word|
+        word.sub!(/^[a-z]/, &:upcase)
+      end
+      
+      words.unshift(first_word).join
+    end
   end
 end
--- spec/vkontakte_api/method_spec.rb
+++ spec/vkontakte_api/method_spec.rb
@@ -1,5 +1,31 @@
 require 'spec_helper'
 
 describe VkontakteApi::Method do
-  # nothing to spec here yet
+  describe "#full_name" do
+    before(:each) do
+      resolver = Hashie::Mash.new(:name => 'name_space')
+      @name = 'name'
+      @method = VkontakteApi::Method.new(@name, :resolver => resolver)
+    end
+    
+    it "sends each part to #camelize" do
+      @method.send(:full_name).should == 'nameSpace.name'
+    end
+  end
+  
+  describe "#type" do
+    context "with a usual name" do
+      it "returns :anything" do
+        method = VkontakteApi::Method.new('get')
+        method.send(:type).should == :anything
+      end
+    end
+    
+    context "with a predicate name" do
+      it "returns :boolean" do
+        method = VkontakteApi::Method.new('is_app_user?')
+        method.send(:type).should == :boolean
+      end
+    end
+  end
 end
