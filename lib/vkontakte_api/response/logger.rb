1beb3295dc6728536fd50ecd2c2a6cbe11b7d345
Leonid Batizhevsky <the.leonko@gmail.com>
2014-03-28 10:36:23 +0400

    fix logger tests

--- lib/vkontakte_api/response/logger.rb
+++ lib/vkontakte_api/response/logger.rb
@@ -25,9 +25,9 @@ module VkontakteApi
     # @param [Hash] env Response data.
     def on_complete(env)
       if env[:body].error?
-        @logger.warn env[:raw_body]  if VkontakteApi.log_errors?
+        @logger.warn env[:body].to_json  if VkontakteApi.log_errors?
       else
-        @logger.debug env[:raw_body] if VkontakteApi.log_responses?
+        @logger.debug env[:body].to_json if VkontakteApi.log_responses?
       end
     end
   end
